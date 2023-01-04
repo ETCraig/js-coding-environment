54edf5ccd92fa15022c252ab6f2b7ecaa1f31201
Craig <dev.etcraig@gmail.com>
2023-01-04 14:30:26 -0700

    Styling & Logic for Monaco Editor Added - Dependency Bug Resolved

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,62 +1,85 @@
-import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
-import prettier from 'prettier';
-import parser from 'prettier/parser-babel';
-import { useRef } from 'react';
+import "./code-editor.css";
+import './syntax.css';
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import { useRef } from "react";
+import codeShift from "jscodeshift";
+import Highlighter from "monaco-jsx-highlighter";
 
 interface CodeEditorProps {
-    intitalValue: string;
-    onChange(value: string): void;
+  intitalValue: string;
+  onChange(value: string): void;
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ intitalValue, onChange }) => {
+  const editorRef = useRef<any>();
 
-    const editorRef = useRef<any>();
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
 
-    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-        editorRef.current = monacoEditor;
-        monacoEditor.onDidChangeModelContent(() => {
-            onChange(getValue());
-        });
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
 
-        monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
-    }
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor
+    );
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
+  };
 
-    const onFormatClick = () => {
-        const unformatted = editorRef.current.getModal().getValue();
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModal().getValue();
 
-        const formatted = prettier.format(unformatted, {
-            parser: 'babel',
-            plugins: [parser],
-            useTabs: false,
-            semi: true,
-            singleQuote: true,
-        });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
 
-        editorRef.current.setValue(formatted);
-    }
+    editorRef.current.setValue(formatted);
+  };
 
-    return (
-    <div>
-        <button onClick={onFormatClick}>Format</button>
-    <MonacoEditor 
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
         value={intitalValue}
         editorDidMount={onEditorDidMount}
-        language="javascript" 
-        theme="dark" 
-        height="500px" 
+        language="javascript"
+        theme="dark"
+        height="500px"
         options={{
-            wordWrap: 'on',
-            minimap: { enabled: false },
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true,
-        }} 
-    />
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
     </div>
-)
+  );
 };
 
 export default CodeEditor;
