0ddc5888100bd661d2c043bcc42c13d87e0a33ec
Craig <dev.etcraig@gmail.com>
2023-01-05 12:28:10 -0700

    Cells Reducer Built Out

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,7 @@
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
+import produce from "immer";
 
 interface CellsState {
   loading: boolean;
@@ -18,29 +19,52 @@ const intialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = intialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = intialState, action: Action) => {
   switch (action.type) {
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
       return state;
     case ActionType.DELETE_CELL:
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
       return state;
     case ActionType.INSERT_CELL_BEFORE:
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+      const currentIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+
+      if (currentIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(currentIndex, 0, cell.id);
+      }
       return state;
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: { ...state.data[id], content: content },
-        },
-      };
+      state.data[id].content = content;
+      return state;
     default:
       return state;
   }
+});
+
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
 };
 
 export default reducer;
